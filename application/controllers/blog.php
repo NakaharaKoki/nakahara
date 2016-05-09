fc51b7475f5d7b77bbc888b0aaddd6a8550b3a76
Nakahara <Nakahara>
2016-05-09 17:09:58 +0900

    0509

---
+++ application/controllers/blog.php
@@ -0,0 +1,57 @@
+<?php
+class Blog extends CI_Controller {
+
+	public function index()
+	{
+		 echo 'Hello World!';
+	}
+	public function test()
+	{
+		$this->load->view('blogview');
+		//echo 'welcom!'; // echo による標準出力
+		//$abc = "test"; // abcという変数に値を格納しているだけ　（内部処理）
+		//$def = "test2"; 
+		//echo $abc . $def; // abcという変数に格納された値を標準出力
+	}
+
+	public function functest()
+	{
+		$this->testEcho('ZZZZZZ');
+	}
+
+	function testEcho($input) {
+		echo $input;
+	}
+
+	public function functest2()
+	{
+		$x = $this->calcAdd(5 , 10);
+		echo $x;
+	}
+
+	function calcAdd($x, $y) {
+		return $x + $y;
+	}
+
+
+	public function functest3()
+	{
+		// $x = $this->calcAdd2(5 , 10);
+		// echo $x;
+		$a = 1;
+		$b = 1;
+		$a = $this->input->post('example1');
+		$b = $this->input->post('example2');
+		$x = $this->calcAdd2($a , $b);
+		echo $a;
+		echo $b;
+		echo "<br>";
+		echo $a . '*' . $b . '=' ;
+		echo $x;
+	}
+
+	function calcAdd2($x, $y) {
+		return $x * $y;
+	}
+}
+?>
---
+++ application/views/blogview.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>My Blog</title>
+</head>
+  <body>
+	<form method="post" action="http://localhost/Code3/index.php/blog/functest3">
+	   <p><input type="text" name="example1"></p>
+	   <p><input type="text" name="example2"></p>
+       <p><input type="submit" value="ボタン"></p>
+    </form>
+  </body>
+</html>
